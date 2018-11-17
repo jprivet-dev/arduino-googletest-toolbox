b498800d70d8d79133bdb1bfa8b1158e18d11d5f
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-11-17 15:38:03 +0100

    dev(basic-test.cpp): extract squareRoot function

--- arduino-project/tests/basic-test.cpp
+++ arduino-project/tests/basic-test.cpp
@@ -1,20 +1,6 @@
-#include <math.h>
-
-// Get the Square root of a number.
-double squareRoot(const double a)
-{
-    double b = sqrt(a);
-    if (b != b) // NaN check
-    {
-        return -1.0;
-    }
-    else
-    {
-        return sqrt(a);
-    }
-}
 
 #include <gtest/gtest.h>
+#include "../src/helpers/squareRoot.cpp"
 
 TEST(SquareRootTest, PositiveNos)
 {
