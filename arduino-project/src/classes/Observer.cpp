091cd00a1a51ef87590298a29e1c45e1ec0e8478
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-11-23 22:46:41 +0100

    dev(Observer.cpp): create base of class

---
+++ arduino-project/src/classes/Observer.cpp
@@ -0,0 +1,20 @@
+#ifndef OBSERVER_CPP
+#define OBSERVER_CPP
+
+class Observer
+{
+  public:
+    Observer()
+    {
+    }
+
+    update()
+    {
+    }
+
+    reportError(errorCode, errorMessage, subject)
+    {
+    }
+};
+
+#endif
