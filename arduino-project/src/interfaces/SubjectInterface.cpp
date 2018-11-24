50662faa31475b72dded24418426a156728b349e
jprivet-dev <jean.philippe.rivet@gmail.com>
2018-11-24 19:00:50 +0100

    dev(SubjectInterface.cpp): create base of interface

---
+++ arduino-project/src/interfaces/SubjectInterface.cpp
@@ -0,0 +1,13 @@
+#ifndef SUBJECT_INTERFACE_CPP
+#define SUBJECT_INTERFACE_CPP
+
+#include <iostream>
+
+struct SubjectInterface
+{
+public:
+  virtual ~SubjectInterface() {}
+  virtual std::string getName() = 0;
+};
+
+#endif
