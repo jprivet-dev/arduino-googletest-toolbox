06bf6b94a46a62c3b3f9b7efae08e14ccd538438
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-04-22 18:28:48 +0200

    dev(Subject.cpp): attach() > basic usage of _observers.push_back()

--- arduino-project/src/classes/Subject.cpp
+++ arduino-project/src/classes/Subject.cpp
@@ -1,6 +1,10 @@
 #ifndef SUBJECT_CPP
 #define SUBJECT_CPP
 
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
 #include "../interfaces/SubjectInterface.cpp"
 #include "Observer.cpp"
 
@@ -8,6 +12,9 @@ class Subject : public SubjectInterface
 {
   protected:
     std::string name;
+
+  private:
+    std::vector<std::string> _observers;
 
   public:
     Subject(std::string _name) : name(_name) {}
@@ -17,7 +24,9 @@ class Subject : public SubjectInterface
       return name;
     }
 
-    void attach() {}
+    void attach() {
+      _observers.push_back("abc");
+    }
 
     void detach() {}
 
