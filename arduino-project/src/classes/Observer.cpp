c40f9e564b2d3964f0d5ca8f96a366bc1c95c43b
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-04-30 13:23:12 +0200

    dev.debug(Observer.cpp): insert 'class observer' WIP

--- arduino-project/src/classes/Observer.cpp
+++ arduino-project/src/classes/Observer.cpp
@@ -9,4 +9,17 @@ class Observer
     void update() {}
 };
 
+class observer
+{
+  public:
+    virtual void notify() = 0;
+};
+
+class observer_concrete : public observer
+{
+  public:
+    virtual void notify() override
+    { }
+};
+
 #endif
