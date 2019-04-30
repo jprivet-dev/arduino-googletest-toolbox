31057d069f7dc8828c58037e11c8692d70850464
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-04-30 13:33:03 +0200

    dev(SubjectTest.cpp): attach_no_observer > test sub->observersObj.size()

--- arduino-project/tests/classes/SubjectTest.cpp
+++ arduino-project/tests/classes/SubjectTest.cpp
@@ -24,6 +24,7 @@ TEST_F(SubjectTestSetup, attach_no_observer)
 {
     // Assert
     ASSERT_EQ(sub->observers.size(), 0);
+    ASSERT_EQ(sub->observersObj.size(), 0);
 }
 
 TEST_F(SubjectTestSetup, attach_1_observer)
