a18bc91ccece71f9b95c5d86a049f290bda4e405
jprivet-dev <jean.philippe.rivet@gmail.com>
2019-05-03 13:33:51 +0200

    dev.refactor(Subject.cpp): use __Observer instead of __observer

--- arduino-project/src/classes/Subject.cpp
+++ arduino-project/src/classes/Subject.cpp
@@ -15,9 +15,9 @@ using namespace std;
 class Subject : public SubjectInterface
 {
   public:
-    vector<reference_wrapper<__observer>> observersObj;
+    vector<reference_wrapper<__Observer>> observersObj;
     vector<string> observers;
-    list<__observer*> observersList;
+    list<__Observer*> observersList;
 
   public:
     Subject() {}
@@ -26,14 +26,14 @@ class Subject : public SubjectInterface
       observers.push_back(observer);
     }
 
-    void attachObj(__observer *o)
+    void attachObj(__Observer *o)
     {
       observersList.push_back(o);
     }
     
     void notify_observersObj()
     {
-      for (__observer& o : observersObj) {
+      for (__Observer& o : observersObj) {
         o.notify();
       }
     }
